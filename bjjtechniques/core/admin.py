1863f7308d500d95a229f7c8df089589ba2b2b8a
Izan Mata <matadiazizan@gmail.com>
2025-10-25 10:52:53 +0200

    Init project

--- bjjtechniques/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Belt, Technique, Transition, Finalization
-
-admin.site.register(Belt)
-admin.site.register(Technique)
-admin.site.register(Transition)
-admin.site.register(Finalization)
