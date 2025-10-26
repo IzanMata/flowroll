8b47c11351a34425847d7e1a37fd39115dcb3953
Izan Mata <matadiazizan@gmail.com>
2025-10-26 10:05:26 +0100

    Init project

--- techniques/urls.py
+++ techniques/urls.py
@@ -1,11 +1,12 @@
 # techniques/urls.py
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+
 from .views import TechniqueViewSet
 
 router = DefaultRouter()
-router.register(r'techniques', TechniqueViewSet)
+router.register(r"techniques", TechniqueViewSet)
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path("api/", include(router.urls)),
 ]
